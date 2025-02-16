cd9fb43f3fa53caf7a48245afd14e9820e12a234
Brandon Simoncic <brandonsimoncic@gmail.com>
2025-02-16 16:34:58 -0500

    Working Camera to topic

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 use opencv::prelude::*;
-use opencv::{highgui, videoio, Result};
+use opencv::{videoio, Result};
 mod calibration;
 
 use rclrs::{create_node, Context, Node, Publisher, RclrsError, QOS_PROFILE_DEFAULT};
 use std::{env, sync::Arc, thread, time::Duration};
-use std_msgs::msg::String as StringMsg;
 use sensor_msgs::msg::Image as ImageMsg;
-use rclrs::msg::image::Image;
 
-
+use builtin_interfaces::msg::Time;
 
 
 struct CameraDriverNode {
@@ -19,26 +17,44 @@ impl CameraDriverNode {
     fn new(context: &Context) -> Result<Self, RclrsError> {
         let node = create_node(context, "camera_node").unwrap();
         let publisher = node
-            .create_publisher("publish_hello", QOS_PROFILE_DEFAULT)
+            .create_publisher("stereo_image", QOS_PROFILE_DEFAULT)
             .unwrap();
         Ok(Self { node, publisher })
     }
-    fn publish_data(&self, increment: i32) -> Result<i32, RclrsError> {
-        let msg: StringMsg = StringMsg {
-            data: format!("Hello World {}", increment),
+    fn publish_data(&self, image: Mat) -> Result<Mat, RclrsError> {
+        let mut msg: ImageMsg = ImageMsg::default();
+		let now = self.node.get_clock().now();
+        msg.header.stamp =  Time {
+            sec: (now.nsec / 1000000000) as i32,
+            nanosec: now.nsec as u32,
         };
+		msg.header.frame_id = "camera_frame".to_string();
+		msg.height = image.rows() as u32;
+		msg.width = image.cols() as u32;
+		msg.encoding = "bgr8".to_string();
+		msg.is_bigendian = 0;
+		msg.step = (image.cols() * 3) as u32;
+		msg.data = image.data_bytes().unwrap().to_vec();
         self.publisher.publish(msg).unwrap();
-        Ok(increment + 1_i32)
+        Ok(image)
     }
 }
 fn main() -> Result<(), RclrsError> {
     let context = Context::new(env::args()).unwrap();
-    let publisher = Arc::new(SimplePublisherNode::new(&context).unwrap());
+    let publisher = Arc::new(CameraDriverNode::new(&context).unwrap());
     let publisher_other_thread = Arc::clone(&publisher);
-    let mut count: i32 = 0;
+	let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY).unwrap(); // 0 is the default camera
+	let opened = videoio::VideoCapture::is_opened(&cam).unwrap();
+	if !opened {
+		panic!("Unable to open default camera!");
+	}
+
+
     thread::spawn(move || loop {
-        thread::sleep(Duration::from_millis(1000));
-        count = publisher_other_thread.publish_data(count).unwrap();
+        thread::sleep(Duration::from_millis(33));
+ 		let mut frame = Mat::default();
+		cam.read(&mut frame).unwrap();
+		frame = publisher_other_thread.publish_data(frame).unwrap();
     });
     rclrs::spin(publisher.node.clone())
 }
